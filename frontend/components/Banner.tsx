efc088ac9e4f1315fd1027cbcfb805c44eb57370
Aryainguz <aryaninguz369@gmail.com>
2024-11-06 07:01:25 +0530

    feat: added banner component

---
+++ frontend/components/Banner.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState } from "react";
+
+
+const Banner = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className="mt-6 text-sm">
+    <div className="w-full h-[0.4px] bg-gradient-to-r from-transparent via-white to-transparent"></div>
+      <div className="flex items-center justify-between px-4 py-6 mx-auto max-w-7xl">
+        <div className="flex gap-4">
+        <h2 className="text-white font-bold relative group
+        cursor-pointer
+        ">
+    BEATS
+    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+  </h2>
+
+
+          
+  <h2 className="text-white font-bold relative group cursor-pointer">
+    GENRES
+    <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
+  </h2>
+
+        </div>
+      </div>
+      <div className="w-full h-[0.8px] bg-gradient-to-r from-transparent via-white to-transparent"></div>
+
+</div>
+  );
+};
+
+export default Banner;
